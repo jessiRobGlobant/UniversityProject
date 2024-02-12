5a135249b3e8cc9c0be6377174b1d1ed860def70
Jessica Robles <jessica.robles@globant.com>
2024-02-12 18:00:01 -0500

    Implemented logic
    Implemented the logic on model classes to print the information of students, teachers and classes

--- src/model/Class.java
+++ src/model/Class.java
@@ -1,11 +1,55 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Class {
     
+    // Attributes
     private String name;
     private String classroom;
     private long teacher;
-    private List<Long> students;
+    private List<Long> students = new ArrayList<>();
+
+    // Constructor
+    public Class(String name, String classroom, long teacher, List<Long> students) {
+        this.name = name;
+        this.classroom = classroom;
+        this.teacher = teacher;
+        this.students = students;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getClassroom() {
+        return classroom;
+    }
+    public void setClassroom(String classroom) {
+        this.classroom = classroom;
+    }
+    public long getTeacher() {
+        return teacher;
+    }
+    public void setTeacher(long teacher) {
+        this.teacher = teacher;
+    }
+    public List<Long> getStudents() {
+        return students;
+    }
+    public void setStudents(List<Long> students) {
+        this.students = students;
+    }
+
+    // Display info
+    public String info(){
+        return String.format("Nombre: %s\nSalon: %s", 
+                    getName(), getClassroom());
+    }
+    
+
 }
--- src/model/Student.java
+++ src/model/Student.java
@@ -6,5 +6,41 @@ public class Student {
     private long id;
     private String name;
     private byte age;
-    private static int count;
+    private static int count = 1;
+
+    // Constructor
+    public Student(String name, byte age) {
+        this.id = count;
+        this.name = name;
+        this.age = age;
+        count ++;
+    }
+
+    // Getters and Setters
+    protected String getName() {
+        return name;
+    }
+
+    protected void setName(String name) {
+        this.name = name;
+    }
+
+    protected byte getAge() {
+        return age;
+    }
+
+    protected void setAge(byte age) {
+        this.age = age;
+    }
+
+    protected long getId() {
+        return id;
+    }
+
+    // Display info
+    public String info(){
+        return String.format("Id: %d\nNombre: %s\nEdad: %d", 
+                    getId(), getName(), getAge());
+    }
+    
 }
